fde28b29174e594eacb13fad2e7a4b6743c8ec9a
Erim <ermvrs@live.com>
2022-11-28 18:35:42 +0300

    result

--- formulas.py
+++ formulas.py
@@ -24,3 +24,8 @@ print(tf)
 
 f = open("result.txt", "w")
 f.write(str(tf))
+
+#result arrayinde sıralama şöyle
+# ARRAY[SATIR][PROPERTY] => PROPERTY SIRALAMASI time, [Ssu Saa Sfa Sva Sbu Spro Sac Sh2 Sch4 SIC SIN SI Xxc Xch Xpr Xli Xsu Xaa  Xfa Xc4 Xpro Xac Xh2 XI Scat San Shva Shbu Shpro...
+# Shac Shco3 Snh3 S_H_ion S_gas_h2 S_gas_ch4 S_gas_co2 q_gas Xhomo XCE Slac Sca] 
+# İlk property time
